0b00ec7901e363c19732efab05c58407067643b3
SantiagoQuishpe312 <sdquishpe1@espe.edu.ec>
2024-07-04 07:27:07 -0500

    initial scene
    initial scene

---
+++ Assets/MenuController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) // Detecta si se presiona la tecla Enter
+        {
+            IniciarJuego();
+        }
+    }
+
+    public void IniciarJuego()
+    {
+        SceneManager.LoadScene("SampleScene"); // Asegúrate de que el nombre de la escena coincida con el que deseas cargar
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0f);
+        transform.position += movement * speed * Time.deltaTime;
+    }
+}
