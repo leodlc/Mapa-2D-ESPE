856e06fababaa627a77f0aac24016c7a70e2da72
SantiagoQuishpe312 <sdquishpe1@espe.edu.ec>
2024-08-28 22:21:00 -0500

    correcciones

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,40 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class GameManager : MonoBehaviour
 {
     public GameObject PipesHolder;
     public GameObject[] Pipes;
-    
+
     [SerializeField]
     int totalPipes = 0;
     [SerializeField]
-    int correctedPipes=0;
+    int correctedPipes = 0;
 
     void Start()
     {
         totalPipes = PipesHolder.transform.childCount;
         Pipes = new GameObject[totalPipes];
 
-        for (int i = 0; i < Pipes.Length; i++) // Corregí "Lenght" a "Length"
+        for (int i = 0; i < Pipes.Length; i++)
         {
             Pipes[i] = PipesHolder.transform.GetChild(i).gameObject;
         }
     }
 
-    // Update is called once per frame
-    public void correctMove(){
-        correctedPipes+=1;
-        Debug.Log("correct move"+totalPipes+" - "+correctedPipes);
-        if (correctedPipes==totalPipes){
-            Debug.Log("You Win!"+totalPipes+" - "+correctedPipes);
-             SceneManager.UnloadSceneAsync("Puzzle2");
+    // Method to call when a pipe is correctly placed
+    public void CorrectMove()
+    {
+        correctedPipes += 1;
+        Debug.Log("Correct move: " + totalPipes + " - " + correctedPipes);
+        if (correctedPipes == totalPipes)
+        {
+            Debug.Log("You Win! " + totalPipes + " - " + correctedPipes);
+            SceneManager.UnloadSceneAsync("Puzzle2");
         }
     }
 
-    public void wrongMove(){
-        correctedPipes -=1;
+    // Method to call when a pipe is incorrectly placed
+    public void WrongMove()
+    {
+        correctedPipes -= 1;
     }
-
-
 }
